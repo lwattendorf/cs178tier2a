f9acaa9d184b8b0302149e38a35c5e0b7ab6054b
Ty Geri <tygeri@college.harvard.edu>
2023-03-04 13:11:17 -0500

    changes to json

--- app/src/components/Main.svelte
+++ app/src/components/Main.svelte
@@ -1,14 +1,14 @@
 <script>
     import Profile from './Profile.svelte'
     import Menubar from './Menubar.svelte'
-    import ContextMenu from './ContextMenu.svelte'
     import { Grid } from 'carbon-components-svelte'
+    import Calendar from './Calendar.svelte';
 </script>
 
 <section>
     <Grid>
         <Profile></Profile>
         <Menubar></Menubar>
-        <ContextMenu></ContextMenu>
     </Grid>
 </section>
+<Calendar></Calendar>
