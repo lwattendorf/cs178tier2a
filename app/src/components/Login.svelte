c47140ddbd06c26679ec9cfbe909362e65928862
Lauren Wattendorf <lwattendorf@college.harvard.edu>
2023-03-05 08:49:05 -0500

    timezone and login auth

--- app/src/components/Login.svelte
+++ app/src/components/Login.svelte
@@ -1,114 +1,126 @@
 <script>
-    import { auth, userDoc } from '../firebase.js';
-    import { Button, Grid, Row, Column, TextInput } from "carbon-components-svelte";
+    import { auth } from '../firebase.js';
+    import { Button, Grid, Row, Column, TextInput, Select, SelectItem } from "carbon-components-svelte";
     import { goto } from "$app/navigation";
 
     let displayName;
+    let selectedTimeZone;
+    let invalidDisplayName = false;
 
     async function signInAnonymously() {
-        console.log("Signing in...");
-        try {
-            const userCredential = await auth.signInAnonymously();
-            const user = userCredential.user;
+
+        console.log(displayName);
+        if (displayName === "") {
+            invalidDisplayName = true;
+        }
+        else {
+            console.log("Signing in...");
+            try {
+                const userCredential = await auth.signInAnonymously();
+                const user = userCredential.user;
 
 
-            // Update the user's profile with the provided name
-            await user.updateProfile({
-                displayName: displayName,
-            });
+                // Update the user's profile with the provided name
+                await user.updateProfile({
+                    displayName: displayName,
+                });
 
-            await goto('/main');
+                await goto('/main');
 
-            console.log('Signed in anonymously as', user.displayName);
-        } catch (error) {
-            console.error('Error signing in', error);
+                console.log('Signed in anonymously as', user.displayName);
+            } catch (error) {
+                console.error('Error signing in', error);
+            }
         }
 	}
 </script>
 
 <Grid>
     <Row padding style="padding-top: 20%" >
-        <Column/>
-        <Column><h1>Welcome</h1></Column>
-        <Column/>
+        <Column sm={{ span: 2, offset: 1 }}><h1>Welcome</h1></Column>
     </Row>
     <Row padding>
-        <Column/>
-        <Column><h4>Sign In</h4></Column>
-        <Column/>
+        <Column sm={{ span: 2, offset: 1 }}><h4>Sign In</h4></Column>
     </Row>
     <Row>
-        <Column/>
-        <Column>
+        <Column sm={{ span: 2, offset: 1 }}>
             <TextInput 
                 bind:value={displayName}
                 id="displayName"
                 placeholder="Your Name" 
+                invalid={invalidDisplayName}
+                invalidText="Please input your name."
+                on:change={() => {if (displayName !== "") { invalidDisplayName = false}}}
             />
         </Column>
     </Row>
     <Row>
-        <Column/>
-        <select name="timezone_offset" id="timezone-offset" class="span5">
-            <option value="-12:00">(GMT -12:00) Eniwetok, Kwajalein</option>
-            <option value="-11:00">(GMT -11:00) Midway Island, Samoa</option>
-            <option value="-10:00">(GMT -10:00) Hawaii</option>
-            <option value="-09:50">(GMT -9:30) Taiohae</option>
-            <option value="-09:00">(GMT -9:00) Alaska</option>
-            <option value="-08:00">(GMT -8:00) Pacific Time (US &amp; Canada)</option>
-            <option value="-07:00">(GMT -7:00) Mountain Time (US &amp; Canada)</option>
-            <option value="-06:00">(GMT -6:00) Central Time (US &amp; Canada), Mexico City</option>
-            <option value="-05:00">(GMT -5:00) Eastern Time (US &amp; Canada), Bogota, Lima</option>
-            <option value="-04:50">(GMT -4:30) Caracas</option>
-            <option value="-04:00">(GMT -4:00) Atlantic Time (Canada), Caracas, La Paz</option>
-            <option value="-03:50">(GMT -3:30) Newfoundland</option>
-            <option value="-03:00">(GMT -3:00) Brazil, Buenos Aires, Georgetown</option>
-            <option value="-02:00">(GMT -2:00) Mid-Atlantic</option>
-            <option value="-01:00">(GMT -1:00) Azores, Cape Verde Islands</option>
-            <option value="+00:00" selected="selected">(GMT) Western Europe Time, London, Lisbon, Casablanca</option>
-            <option value="+01:00">(GMT +1:00) Brussels, Copenhagen, Madrid, Paris</option>
-            <option value="+02:00">(GMT +2:00) Kaliningrad, South Africa</option>
-            <option value="+03:00">(GMT +3:00) Baghdad, Riyadh, Moscow, St. Petersburg</option>
-            <option value="+03:50">(GMT +3:30) Tehran</option>
-            <option value="+04:00">(GMT +4:00) Abu Dhabi, Muscat, Baku, Tbilisi</option>
-            <option value="+04:50">(GMT +4:30) Kabul</option>
-            <option value="+05:00">(GMT +5:00) Ekaterinburg, Islamabad, Karachi, Tashkent</option>
-            <option value="+05:50">(GMT +5:30) Bombay, Calcutta, Madras, New Delhi</option>
-            <option value="+05:75">(GMT +5:45) Kathmandu, Pokhara</option>
-            <option value="+06:00">(GMT +6:00) Almaty, Dhaka, Colombo</option>
-            <option value="+06:50">(GMT +6:30) Yangon, Mandalay</option>
-            <option value="+07:00">(GMT +7:00) Bangkok, Hanoi, Jakarta</option>
-            <option value="+08:00">(GMT +8:00) Beijing, Perth, Singapore, Hong Kong</option>
-            <option value="+08:75">(GMT +8:45) Eucla</option>
-            <option value="+09:00">(GMT +9:00) Tokyo, Seoul, Osaka, Sapporo, Yakutsk</option>
-            <option value="+09:50">(GMT +9:30) Adelaide, Darwin</option>
-            <option value="+10:00">(GMT +10:00) Eastern Australia, Guam, Vladivostok</option>
-            <option value="+10:50">(GMT +10:30) Lord Howe Island</option>
-            <option value="+11:00">(GMT +11:00) Magadan, Solomon Islands, New Caledonia</option>
-            <option value="+11:50">(GMT +11:30) Norfolk Island</option>
-            <option value="+12:00">(GMT +12:00) Auckland, Wellington, Fiji, Kamchatka</option>
-            <option value="+12:75">(GMT +12:45) Chatham Islands</option>
-            <option value="+13:00">(GMT +13:00) Apia, Nukualofa</option>
-            <option value="+14:00">(GMT +14:00) Line Islands, Tokelau</option>
-        </select>
-        <Column/>
+        <Column sm={{ span: 2, offset: 1 }}>
+        <Select name="timezone_offset" selected="-05:00" bind:selectedTimeZone>
+            <SelectItem value="-12:00" text="(GMT -12:00) Eniwetok, Kwajalein" />
+            <SelectItem value="-11:00" text="(GMT -11:00) Midway Island, Samoa" />
+            <SelectItem value="-10:00" text="(GMT -10:00) Hawaii" />
+            <SelectItem value="-09:50" text="(GMT -9:30) Taiohae" />
+            <SelectItem value="-09:00" text="(GMT -9:00) Alaska" />
+            <SelectItem value="-08:00" text="(GMT -8:00) Pacific Time (US &amp; Canada)" />
+            <SelectItem value="-07:00" text="(GMT -7:00) Mountain Time (US &amp; Canada)" />
+            <SelectItem value="-06:00" text="(GMT -6:00) Central Time (US &amp; Canada), Mexico City" />
+            <SelectItem value="-05:00" text="(GMT -5:00) Eastern Time (US &amp; Canada), Bogota, Lima" />
+            <SelectItem value="-04:50" text="(GMT -4:30) Caracas" />
+            <SelectItem value="-04:00" text="(GMT -4:00) Atlantic Time (Canada), Caracas, La Paz" />
+            <SelectItem value="-03:50" text="(GMT -3:30) Newfoundland" />
+            <SelectItem value="-03:00" text="(GMT -3:00) Brazil, Buenos Aires, Georgetown" />
+            <SelectItem value="-02:00" text="(GMT -2:00) Mid-Atlantic" />
+            <SelectItem value="-01:00" text="(GMT -1:00) Azores, Cape Verde Islands" />
+            <SelectItem value="+00:00" text="(GMT) Western Europe Time, London, Lisbon, Casablanca" />
+            <SelectItem value="+01:00" text="(GMT +1:00) Brussels, Copenhagen, Madrid, Paris" />
+            <SelectItem value="+02:00" text="(GMT +2:00) Kaliningrad, South Africa" />
+            <SelectItem value="+03:00" text="(GMT +3:00) Baghdad, Riyadh, Moscow, St. Petersburg" />
+            <SelectItem value="+03:50" text="(GMT +3:30) Tehran" />
+            <SelectItem value="+04:00" text="(GMT +4:00) Abu Dhabi, Muscat, Baku, Tbilisi" />
+            <SelectItem value="+04:50" text="(GMT +4:30) Kabul" />
+            <SelectItem value="+05:00" text="(GMT +5:00) Ekaterinburg, Islamabad, Karachi, Tashkent" />
+            <SelectItem value="+05:50" text="(GMT +5:30) Bombay, Calcutta, Madras, New Delhi" />
+            <SelectItem value="+05:75" text="(GMT +5:45) Kathmandu, Pokhara" />
+            <SelectItem value="+06:00" text="(GMT +6:00) Almaty, Dhaka, Colombo" />
+            <SelectItem value="+06:50" text="(GMT +6:30) Yangon, Mandalay" />
+            <SelectItem value="+07:00" text="(GMT +7:00) Bangkok, Hanoi, Jakarta" />
+            <SelectItem value="+08:00" text="(GMT +8:00) Beijing, Perth, Singapore, Hong Kong" />
+            <SelectItem value="+08:75" text="(GMT +8:45) Eucla" />
+            <SelectItem value="+09:00" text="(GMT +9:00) Tokyo, Seoul, Osaka, Sapporo, Yakutsk" />
+            <SelectItem value="+09:50" text="(GMT +9:30) Adelaide, Darwin" />
+            <SelectItem value="+10:00" text="(GMT +10:00) Eastern Australia, Guam, Vladivostok" />
+            <SelectItem value="+10:50" text="(GMT +10:30) Lord Howe Island" />
+            <SelectItem value="+11:00" text="(GMT +11:00) Magadan, Solomon Islands, New Caledonia" />
+            <SelectItem value="+11:50" text="(GMT +11:30) Norfolk Island" />
+            <SelectItem value="+12:00" text="(GMT +12:00) Auckland, Wellington, Fiji, Kamchatka" />
+            <SelectItem value="+12:75" text="(GMT +12:45) Chatham Islands" />
+            <SelectItem value="+13:00" text="(GMT +13:00) Apia, Nukualofa" />
+            <SelectItem value="+14:00" text="(GMT +14:00) Line Islands, Tokelau" />
+        </Select>
+        </Column>
     </Row>
     <Row>
-        <Column/>
-        <Column>
+        <Column sm={{ span: 2, offset: 1 }}>
             <TextInput 
                 placeholder="Password (optional)" 
                 id="password" 
                 type="password" 
                 name="password" />
         </Column>
-        <Column/>
     </Row>
     <Row padding>
-        <Column/>
-        <Column>
+        <Column sm={{ span: 2, offset: 1 }}>
             <Button style="width: 100%" on:click={signInAnonymously}>Sign in</Button>
         </Column>
-        <Column/>
     </Row>
 </Grid>
+
+<style>
+    :global(.bx--row) {
+        padding-bottom: 1rem;
+    }
+    :global(.bx--select) {
+        margin-top: -0.5rem;
+    }
+</style>
